7ba46481501f3185ba4d27fce22e4adaa5bcb661
张柒 <12909657+love-open-source-white-whore@user.noreply.gitee.com>
2024-08-18 09:28:40 +0800

    111

--- src/main/java/com/hdu/Solution.java
+++ src/main/java/com/hdu/Solution.java
@@ -1,8 +1,23 @@
 package com.hdu;
 
 
-import java.util.*;
-
 class Solution {
-
+    public boolean checkRecord(String s) {
+        char[] chars = s.toCharArray();
+        int countA = 0;
+        int countL = 0;
+        int maxL = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 'A') {
+                countA++;
+            }
+            if (chars[i] == 'L') {
+                if(i==0||chars[i-1]=='L') {
+                    countL++;
+                }else countL=1;
+                maxL = Math.max(maxL,countL);
+            }
+        }
+        return countA <= 1 && maxL <= 2;
+    }
 }
